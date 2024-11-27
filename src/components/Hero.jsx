4621753d5f391a590449cb164f8b1f0a2698ce7c
t-dlsantos <thiagodlsantos9@gmail.com>
2024-11-27 00:51:43 -0300

    feat: comprar produtos

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,19 @@
 import Button from "./Button"
 
 export default function Hero() {
+  const scrollToSection = (id) => {
+    const section = document.getElementById(id);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
   return (
     <>
       <h1 className="font-bold text-5xl w-[720px] leading-snug text-white mb-7">
         Produtos Hipoalergênicos. Cuide da pessoa mais importante, você mesma!!!
       </h1>
       <p className="text-2xl text-white mb-12">Beleza que respeita sua pele.</p>
-      <button className="bg-[#055664] px-10 py-3 text-lg font-bold text-white rounded-xl">Comprar Produtos</button>
+      <button onClick={() => scrollToSection('produtos')} className="bg-[#055664] px-10 py-3 text-lg font-bold text-white rounded-xl">Comprar Produtos</button>
     </>
   );
 }
